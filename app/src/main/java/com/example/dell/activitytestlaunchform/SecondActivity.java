4ead8cb16c0b97c3d395dabe91ecfc46f204dc49
Tim-xiaofan <34571885+Tim-xiaofan@users.noreply.github.com>
2018-03-26 13:46:27 +0800

    ActivityCollector

--- app/src/main/java/com/example/dell/activitytestlaunchform/SecondActivity.java
+++ app/src/main/java/com/example/dell/activitytestlaunchform/SecondActivity.java
@@ -1,5 +1,6 @@
 package com.example.dell.activitytestlaunchform;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +11,14 @@ import android.widget.Button;
 public class SecondActivity extends BaseActivity {
 
     public  static  final String TAG="SecondActivity";
+
+    public static void actionStart(Context context, String data1, String data2) {
+        Intent intent = new Intent(context, SecondActivity.class);
+        intent.putExtra("param1", data1);
+        intent.putExtra("param2", data2);
+        context.startActivity(intent);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d("SecondActivity","Task id is"+getTaskId());
